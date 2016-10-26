a05ea9bf65339c7fafd954f75386a7c66686b49b
Alex Dmitriev <dmitriev_a@rambler.ru>
2016-10-26 13:10:51 +0300

    Playing with IDE

---
+++ src/AppBundle/Controller/MovieController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MovieController extends Controller
+{
+    /**
+     * @Route('/movies/new', name="movies_new")
+     */
+    public function newAction($name)
+    {
+        return $this->render('movie/new.html.twig', array('name' => $name));
+    }
+}
---
+++ src/AppBundle/Entity/Movie.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="movie")
+ */
+class Movie
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isAvailable;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+}
